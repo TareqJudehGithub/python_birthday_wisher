e37a7f0e2d620644824490188e7dc00393960b28
Tareq Judeh <tareq.judeh@outlook.com>
2020-12-29 13:46:11 +0200

    Refactored project to code OOP.

--- main.py
+++ main.py
@@ -1,52 +1,50 @@
-##################### Normal Starting Project ######################
-import datetime as dt
 import pandas
-from random import randint
 import smtplib
-from email.mime.image import MIMEImage
-my_user = "tj.coder1975@gmail.com"
-my_pass = "BlueOcean75@"
+import datetime as dt
+from random import randint
 
-# Instantiate a new object from dt:
-today = dt.datetime.now()
 
-# 2. Check if today matches a birthday in the birthdays.csv
-today_month = today.month
-today_day = today.day
-today_tuple = (today_month, today_day)
+class BirthdayWisher:
+    date = dt.datetime.now()
+    day = date.day
+    month = date.month
+    today_date = (month, day)
 
-# Create a dictionary from birthday.csv
-birthdays = pandas.read_csv("birthdays.csv")
-birthdays_dict = {index: (row["month"], row["day"]) for (index, row) in birthdays.iterrows()}
+    def __init__(self):
+        self.my_user = "Your Email address"
+        self.my_pass = "Your Email password"
+        self.recipients_birthday()
 
-for index in birthdays_dict:
+    def recipients_birthday(self):
+        """Compare birthdays list birthday date with today's:"""
+        birthdays = pandas.read_csv("birthdays.csv")
+        birthdays_dict = {index: (row["month"], row["day"]) for (index, row) in birthdays.iterrows()}
+        for index in birthdays_dict:
+            birthday_date = birthdays_dict[index]
+            recipient_name = birthdays["name"][index]
+            recipient_email = birthdays["email"][index]
 
-    birthday_date = birthdays_dict[index]
-    if birthday_date == today_tuple:
-        birthday_person = birthdays["name"][index]
-        birthday_email = birthdays["email"][index]
+            if self.today_date == birthday_date:
+                # Birthday card body:
+                files_path = f"./letter_templates/letter_{randint(1, 3)}.txt"
+                with open(files_path) as files:
+                    card = files.read()
+                    recipient_card = card.replace("[NAME]", recipient_name)
 
-        # Create a new random birthday card:
-        file_path = f"./letter_templates/letter_{randint(1, 3)}.txt"
-        with open(file_path) as file:
-            letter = file.read()
-            letter = letter.replace("[NAME]", birthday_person)
+                # Send emails to recipients:
+                with smtplib.SMTP("smtp.gmail.com") as connection:
+                    connection.starttls()
+                    connection.login(user=self.my_user, password=self.my_pass)
+                    connection.sendmail(
+                        from_addr=self.my_user,
+                        to_addrs=recipient_email,
+                        msg=f"Subject: Happy Birthday {recipient_name}!\n\n"
+                            f"{recipient_card}".encode("utf8")
+                    )
 
-        with open("./cards_sent/cards.txt", mode="a") as file:
-            file.write(letter)
+                # Save a copy of cards sent in a separate file:
+                with open("cards_sent/cards.txt", mode="w") as file:
+                    file.write(recipient_card)
 
-        # 4. Send the letter generated in step 3 to that person's email address.
-        # User credentials:
-        with smtplib.SMTP("smtp.gmail.com") as connection:
-            # Secure mail server connection:
-            connection.starttls()
 
-            # Login to Gmail:
-            connection.login(user=my_user, password=my_pass)
-
-            # Send email:
-            connection.sendmail(
-                from_addr=my_user,
-                to_addrs=birthday_email,
-                msg=f"Subject: Happy Birthday!\n\n{letter}".encode("utf8")
-            )
+BirthdayWisher()
